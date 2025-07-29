b5f8b05698697e3be2a43677a6d949a3fd5250b6
Hed <hed.barnissan@gmail.com>
2025-07-29 21:42:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ export const PLAYER_COLORS = {
     2: '#000000', // Player 2: Black
 };
 
-export const WINNING_ROW_LENGTH = 3;
+export const WINNING_ROW_LENGTH = 4;
 
 // New constant for falling speed
 export const STONE_FALLING_SPEED = 500; // Speed in pixels per second
